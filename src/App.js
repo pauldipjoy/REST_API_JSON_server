f50c4115c6767407a49f7d48edf9b62ccdab8b37
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-03-31 23:17:57 +0600

    Error Handling , Post , Put , Delete second commit

--- src/App.js
+++ src/App.js
@@ -11,49 +11,28 @@ componentDidMount(){
   // fetch('https://jsonplaceholder.typicode.com/posts')
   // .then(response => response.json())
   // .then(data => console.log(data));
-
-
+  
 
   // axios.get('http://localhost:3001/dishes')
   // .then(response => console.log(response.data))
   // .then(data => console.log(data))
   // .catch(error => console.log(error.message));
 
-
-  // axios.post('http://localhost:3001/dishes' , {name: "Pizzas" , price: "799"})
-  // .then(response => console.log(response) );
-
-// axios.put('http://localhost:3001/dishes/0' , {name : "BURGER" , price: "200" })
-// .then(response => console.log(response));
-
-
-
-// axios.get('http://localhost:3001/dishes')
-// .then(response => console.log(response.data))
-// .then(data => console.log(data))
-
-
-  // axios.post('http://localhost:3001/dishes', {name : "Biriyani" , price : "345"})
+  // axios.post('http://localhost:3001/dishes' , {name: "kacchi" , price: "425"})
   // .then(response => console.log(response));
 
-//   axios.put('http://localhost:3001/dishes/3', {name : "Chawmin" , price : "200"})
-// .then(response => console.log(response));
+  // axios.put('http://localhost:3001/dishes/4' , {name: "Biriyani" , price: "300"})
+  // .then(response => console.log(response));
 
-axios.delete('http://localhost:3001/dishes/2')
-.then(response => console.log(response));
-
-
-
-
-  
+  axios.delete('http://localhost:3001/dishes/3')
+  .then(response => console.log(response));
 
 
 
 
-}  
+  }  
 
-
-  render(){
+render(){
 
     return (
       <div>
