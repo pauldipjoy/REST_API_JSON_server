50298a9cddd7c2f6a3d75fae6bac1a16a14d7bf1
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-03-31 12:33:49 +0600

    Error Handling , Post,Put,Delete

--- src/App.js
+++ src/App.js
@@ -14,14 +14,43 @@ componentDidMount(){
 
 
 
-  axios.get('https://jsonplaceholder.typicode.com/posts')
-  .then(response => console.log(response.data))
-  .then(data => console.log(data));
+  // axios.get('http://localhost:3001/dishes')
+  // .then(response => console.log(response.data))
+  // .then(data => console.log(data))
+  // .catch(error => console.log(error.message));
 
 
-}
+  // axios.post('http://localhost:3001/dishes' , {name: "Pizzas" , price: "799"})
+  // .then(response => console.log(response) );
+
+// axios.put('http://localhost:3001/dishes/0' , {name : "BURGER" , price: "200" })
+// .then(response => console.log(response));
 
 
+
+// axios.get('http://localhost:3001/dishes')
+// .then(response => console.log(response.data))
+// .then(data => console.log(data))
+
+
+  // axios.post('http://localhost:3001/dishes', {name : "Biriyani" , price : "345"})
+  // .then(response => console.log(response));
+
+//   axios.put('http://localhost:3001/dishes/3', {name : "Chawmin" , price : "200"})
+// .then(response => console.log(response));
+
+axios.delete('http://localhost:3001/dishes/2')
+.then(response => console.log(response));
+
+
+
+
+  
+
+
+
+
+}  
 
 
   render(){
