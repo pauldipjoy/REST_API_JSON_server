051154b98d228af4ae3f8c08304a52fc74819f80
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-03-31 09:36:51 +0600

    using REST API from react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+componentDidMount(){
+
+  
+
+  // fetch('https://jsonplaceholder.typicode.com/posts')
+  // .then(response => response.json())
+  // .then(data => console.log(data));
+
+
+
+  axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => console.log(response.data))
+  .then(data => console.log(data));
+
+
+}
+
+
+
+
+  render(){
+
+    return (
+      <div>
+        
+      </div>
+    );
+
+
+  }
+  
 }
 
 export default App;
