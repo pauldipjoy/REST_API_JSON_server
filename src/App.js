ac567761d68ba29b144f4720b6d3d73c581be752
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-04-01 00:29:10 +0600

    Handling Responses using state

--- src/App.js
+++ src/App.js
@@ -4,19 +4,37 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+    dishes: null,
+    errorMess: null
+  }
+
 componentDidMount(){
 
-  
+  console.log("ComponentDidMount:" , this.state);
 
   // fetch('https://jsonplaceholder.typicode.com/posts')
   // .then(response => response.json())
   // .then(data => console.log(data));
   
 
-  // axios.get('http://localhost:3001/dishes')
-  // .then(response => console.log(response.data))
-  // .then(data => console.log(data))
-  // .catch(error => console.log(error.message));
+// axios.get('http://localhost:3001/dishes')
+//   .then(response => response.data)
+//   .then(data => {
+
+//     this.setState({
+
+//       dishes: data
+//     })
+
+//   })
+  
+// .catch(error => {
+//     this.setState({
+//       errorMess: error.message
+//     })
+
+//   });
 
   // axios.post('http://localhost:3001/dishes' , {name: "kacchi" , price: "425"})
   // .then(response => console.log(response));
@@ -25,12 +43,22 @@ componentDidMount(){
   // .then(response => console.log(response));
 
   axios.delete('http://localhost:3001/dishes/3')
-  .then(response => console.log(response));
+  .then(response => console.log(response))
+  .catch(error => {
+    this.setState({
+      errorMess: error.message
+    })
 
+  });
 
+} 
 
+componentDidUpdate(){
 
-  }  
+  console.log("Update :" , this.state);
+  
+}
+
 
 render(){
 
